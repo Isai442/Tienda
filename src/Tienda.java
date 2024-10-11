e1e0619e04579a121772b7862599d52973006363
chenoah <pgarciac10@miumg.edu.gt>
2024-10-11 01:15:39 -0600

    Se agrega el proyecto

---
+++ src/Tienda.java
@@ -0,0 +1,12 @@
+
+public class Tienda {
+
+   
+    public static void main(String[] args) {
+        
+      registoCategoria R = new registoCategoria();
+      R.setVisible(true);
+      // que se muestre la ventana registo Categoria si existe
+    }
+    
+}
